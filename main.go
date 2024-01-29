a392019e42d29af220182f95f417ea4e4aed10c7
Andy Dayton <amdayton@gmail.com>
2024-01-29 21:43:41 +0100

    add flags to main

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"flag"
 	"log"
 
 	"github.com/andyinabox/linkydink/app"
@@ -11,10 +12,16 @@ import (
 var res embed.FS
 
 func main() {
+	var host string
+	var port string
+
+	flag.StringVar(&host, "host", "127.0.0.1", "host to run the webserver on")
+	flag.StringVar(&port, "port", "8000", "port to run the webserver on")
+	flag.Parse()
 
 	appInstance := app.New(&app.Config{
-		Host:      "127.0.0.1",
-		Port:      "8000",
+		Host:      host,
+		Port:      port,
 		Resources: res,
 	})
 
