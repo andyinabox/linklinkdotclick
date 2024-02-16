f3f1300b8b71ec95f1343c4d6d433d52f20c7aa9
Andy Dayton <amdayton@gmail.com>
2024-02-16 09:55:14 +0100

    popup

--- assets/components/elements/form-style-editor.js
+++ assets/components/elements/form-style-editor.js
@@ -3,14 +3,22 @@ import { FormBase } from './form-base'
 export class FormStyleEditor extends FormBase {
   constructor() {
     super()
-
     this.editor = this.querySelector('textarea')
     this.userStyles = document.getElementById('user-styles')
+    this.buttons['btn-styles-open'].hidden = false
     this.socket = new WebSocket(`wss://${window.location.host}/ws/style-editor`)
     this.socket.onerror = (err) => {
       console.error(err)
     }
   }
+  handleNewWindowClick() {
+    this.popup = window.open(
+      '/styles',
+      '_blank',
+      'popup=1,width=500,height=700'
+    )
+  }
+
   handleSocketMessage(msg) {
     console.log('socket message', msg)
     this.editor.value = msg.data
@@ -34,6 +42,12 @@ export class FormStyleEditor extends FormBase {
     this.listen(this.editor, 'keydown', this.handleKeyPress)
     this.listen(this.socket, 'open', this.handleSocketOpen)
     this.listen(this.socket, 'message', this.handleSocketMessage)
+    this.listen(this.socket, 'message', this.handleSocketMessage)
+    this.listen(
+      this.buttons['btn-styles-open'],
+      'click',
+      this.handleNewWindowClick
+    )
   }
 }
 
